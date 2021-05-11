bd7c85984fba98f9aa832580b1d6c3cc69ad50a6
hthhth <54969380+hthhth@users.noreply.github.com>
2021-05-11 10:12:05 +0700

    Init new automation test framework.

---
+++ 1_testcases/com/bankguru/payment/Payment_01_Fund_Transfer.java
@@ -0,0 +1,19 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Payment_01_Fund_Transfer {
+
+    @BeforeClass
+    public void beforeClass(){
+
+    }
+
+
+    @AfterClass
+    public void afterClass(){
+
+    }
+}
---
+++ 1_testcases/com/bankguru/payment/Payment_02_Deposit.java
@@ -0,0 +1,19 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Payment_02_Deposit {
+
+    @BeforeClass
+    public void beforeClass(){
+
+    }
+
+
+    @AfterClass
+    public void afterClass(){
+
+    }
+}
