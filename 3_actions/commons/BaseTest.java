dabebae49138b8e154b890af6c4bb46cdb08ede2
hthhth <54969380+hthhth@users.noreply.github.com>
2021-05-30 17:39:30 +0700

    Level 4 - Multi browser, Factory Pattern, WebDriverManager

--- 3_actions/commons/BaseTest.java
+++ 3_actions/commons/BaseTest.java
@@ -1,24 +1,33 @@
 package commons;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     private WebDriver driver;
+    private enum BROWSER {
+        CHROME, FIREFOX, IE, SAFARI, EDGE_LEGACY, EDGE_CHROMIUM, H_CHROME, H_FIREFOX;
+    }
+
+    private String osName = System.getProperty("os.name");
 
     protected WebDriver getBrowserDriver(String browserName){
-        if (browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", "./browserDrivers/geckodriver.exe");
+        BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
+        if (browser == BROWSER.FIREFOX) {
+            WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
-        } else if (browserName.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "./browserDrivers/chromedriver.exe");
+        } else if (browser == BROWSER.CHROME) {
+            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("edge_chromium")) {
-            System.setProperty("webdriver.edge.driver", "./browserDrivers/msedgedriver.exe");
+        } else if (browser == BROWSER.EDGE_CHROMIUM) {
+            WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
         } else {
             throw new RuntimeException("Please enter correct browser name!");
@@ -26,15 +35,16 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         return driver;
     }
-     protected WebDriver getBrowserDriver(String browserName, String appURL){
-        if (browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", "./browserDrivers/geckodriver.exe");
+    protected WebDriver getBrowserDriver(String browserName, String appURL){
+        BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
+        if (browser == BROWSER.FIREFOX) {
+            WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
-        } else if (browserName.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "./browserDrivers/chromedriver.exe");
+        } else if (browser == BROWSER.CHROME) {
+            WebDriverManager.chromedriver().driverVersion("90.0.4430.93").setup();
             driver = new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("edge_chromium")) {
-            System.setProperty("webdriver.edge.driver", "./browserDrivers/msedgedriver.exe");
+        } else if (browser == BROWSER.EDGE_CHROMIUM) {
+            WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
         } else {
             throw new RuntimeException("Please enter correct browser name!");
@@ -44,4 +54,8 @@ public class BaseTest {
         return driver;
     }
 
+    private String getSlash(String folderName){
+        String separator = File.separator;
+        return separator + folderName + separator;
+    }
 }
