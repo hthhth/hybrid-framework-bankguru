cbe94941a5c523905523de89b9fbb281564af21b
hthhth <54969380+hthhth@users.noreply.github.com>
2021-09-18 20:17:42 +0700

    Level 17 Independence Level 18 Sort Level 19 Data

--- 3_actions/commons/BaseTest.java
+++ 3_actions/commons/BaseTest.java
@@ -87,7 +87,6 @@ public class BaseTest {
             Assert.assertTrue(condition);
         } catch (Throwable e) {
             pass = false;
-
             // Add lỗi vào ReportNG
             VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
             Reporter.getCurrentTestResult().setThrowable(e);
